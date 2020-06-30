5db329f6b874908b940b8c519ed9f2d27c444479
UpsetDavid <57288936+UpsetDavid@users.noreply.github.com>
2020-06-30 02:34:02 -0500

    Update hawku.js

--- src/commands/hawku.js
+++ src/commands/hawku.js
@@ -8,30 +8,27 @@ const options = {compact: true, ignoreDeclaration: true, ignoreAttributes: true,
 // Ran every time this command is ran, in-case the user changes their configuration
 // This loads the entire file into a JS object after converting it
 const loadConfigFile = () => {
-  return xml2js(
-    fs.readFileSync(hawkuConfigFilePath, 'utf8'), options
-  ).Configuration;
-}
+    return xml2js(
+      fs.readFileSync(hawkuConfigFilePath, 'utf8'), options
+    ).Configuration;
+  }
 
 module.exports = {
 
-    GetAreas: function() {
+    getArea: function() {
         // This runs the loadConfigFile function, which returns the entire file as a JS object
-        // It then builds two variables (current settings, and the dimensions of the tablet's full area)
-        const settings = loadConfigFile().TabletArea;
-        const fullArea = loadConfigFile().TabletFullArea;
-
-        let areaWidth = settings.Width._text;
-        let areaHeight = settings.Height._text;
-
-        // place all of these values into a key/value pair, and return back to the parent caller
-        let areas = { width: settings.Width._text, height: settings.Height._text, maxwidth: fullArea.Width._text, maxheight: fullArea.Height._text}
-        return areas;
+        // It then returns the 4 values (current area, and max area) to the caller
+        const settings = loadConfigFile();
+        return { 
+            width: settings.TabletArea.Width._text, 
+            height: settings.TabletArea.Height._text, 
+            maxWidth: settings.TabletFullArea.Width._text, 
+            maxHeight: settings.TabletFullArea.Height._text 
+        };
     },
 
     getDetails: () => {
         const settings = loadConfigFile();
-
         return { 
             forcedAspectRatio: settings.ForceAspectRatio._text === 'true', 
             fullArea: settings.ForceFullArea._text === 'true', 
