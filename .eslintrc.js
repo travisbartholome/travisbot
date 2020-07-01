28d4d81e115e1c53ef6b818db876f4cf8e6ac23a
travisbartholome <travis.bartholome@gmail.com>
2020-07-01 03:21:06 -0500

    Regenerate eslint config to be up-to-date

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: ['airbnb-base'],
   env: {
-    es6: true,
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
   },
   rules: {
     'no-console': 'off',
