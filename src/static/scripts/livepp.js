7ab1feed89689bdb6f14cb2878f4f0f98193d336
travisbartholome <travis.bartholome@gmail.com>
2020-07-26 16:04:07 -0500

    Styling, a bit of code cleanup

--- src/static/scripts/livepp.js
+++ src/static/scripts/livepp.js
@@ -9,7 +9,7 @@ let ppCurrent;
 let ppDisplayArray = [];
 
 const IN_ANIMATION_PROPS = {
-  duration: 200,
+  duration: 100,
   delay: (el, idx) => (el.parentNode.children.length - idx - 1) * 50,
   easing: 'easeInOutQuad',
   opacity: [0, 1],
@@ -19,22 +19,14 @@ const IN_ANIMATION_PROPS = {
 
 const OUT_ANIMATION_PROPS = {
   delay: (el, idx) => (el.parentNode.children.length - idx - 1) * 50,
-  duration: 200,
+  duration: 100,
   easing: 'easeInOutQuad',
   opacity: 0,
   targets: '.pp-digit',
   translateY: '-50%',
 };
 
-// TODO: fix this and actually implement something that works
 function updatePPValue() {
-  if (!ppCurrent || ppCurrent.length === 0) {
-    ppDisplayArray = [];
-    return;
-    // TODO: test this to see what happens at the end of a map
-    // TODO: possibly remove since this never seems to get called
-  }
-
   const ppString = ppCurrent.slice(0, ppCurrent.indexOf('.')); // "123"
   const ppDisplayNewArray = ppString.split(''); // ['1', '2', '3']
 
@@ -52,21 +44,13 @@ function updatePPValue() {
   }
   // Note length of both arrays should now be equal
 
-  // TODO: remove
-  console.log(ppCurrent);
-  console.log(ppString);
-  console.log(ppDisplayArray);
-  console.log(ppDisplayNewArray);
-
-  // TODO: refactor with array.reduce
-  let index = 0;
-  let newHTML = '';
-  while (index < ppDisplayNewArray.length) {
-    // Generate new digit elements to show pp
-    newHTML += `<span class="pp-digit">${ppDisplayNewArray[index]}</span>`;
-
-    index += 1;
-  }
+  // Put each digit in a span
+  const newHTML = ppDisplayNewArray.reduce(
+    (html, currentDigit) => (
+      `${html}<span class="pp-digit">${currentDigit}</span>`
+    ),
+    '',
+  );
 
   // TODO: fix linter (not quite sure what the best strategy is here)
   // TODO: debug weird thing where if the browser window loses focus,
